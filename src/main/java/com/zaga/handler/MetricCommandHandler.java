3ecf5d19831a218ae4c59c42354ff44eadc8482e
Anushiya <anushiyadhamu@gmail.com>
2023-10-05 14:44:45 +0530

    updated the metric marsheller

--- src/main/java/com/zaga/handler/MetricCommandHandler.java
+++ src/main/java/com/zaga/handler/MetricCommandHandler.java
@@ -6,19 +6,15 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.zaga.entity.otelmetric.OtelMetric;
 import com.zaga.entity.otelmetric.ResourceMetric;
 import com.zaga.entity.otelmetric.ScopeMetric;
 import com.zaga.entity.otelmetric.scopeMetric.Metric;
 import com.zaga.entity.otelmetric.scopeMetric.MetricGauge;
-import com.zaga.entity.otelmetric.scopeMetric.MetricHistogram;
 import com.zaga.entity.otelmetric.scopeMetric.MetricSum;
 import com.zaga.entity.otelmetric.scopeMetric.gauge.GaugeDataPoint;
-import com.zaga.entity.otelmetric.scopeMetric.histogram.HistogramDataPoint;
 import com.zaga.entity.otelmetric.scopeMetric.sum.SumDataPoint;
 import com.zaga.entity.queryentity.metrics.MetricDTO;
-import com.zaga.entity.queryentity.trace.TraceDTO;
 import com.zaga.repo.MetricCommandRepo;
 import com.zaga.repo.MetricDTORepo;
 
@@ -37,82 +33,98 @@ public class MetricCommandHandler {
     public void createMetricProduct(OtelMetric metrics) {
         metricCommandRepo.persist(metrics);
         List<MetricDTO> metricDTOs = extractAndMapData(metrics);
-        System.out.println("MetricDTOs: " + metricDTOs);
+        // System.out.println("MetricDTOs: " + metricDTOs);
     }
 
     private List<MetricDTO> extractAndMapData(OtelMetric metrics) {
-    List<MetricDTO> metricDTOs = new ArrayList<>();
-    try {
-        for (ResourceMetric resourceMetric : metrics.getResourceMetrics()) {
-            String serviceName = getServiceName(resourceMetric);
-            for (ScopeMetric scopeMetric : resourceMetric.getScopeMetrics()) {
-                try {
-                    String name = scopeMetric.getScope().getName();
-                    if (name != null && name.contains("io.opentelemetry.runtime")) {
-                        List<Metric> metricsList = scopeMetric.getMetrics();
-                        for (Metric metric : metricsList) {
-                            String metricName = metric.getName();
-                            if (
-                                metricName.equals("process.runtime.jvm.threads.count")
-                                || metricName.equals("process.runtime.jvm.system.cpu.utilization")
-                                || metricName.equals("process.runtime.jvm.system.cpu.load_1m")
-                                || metricName.equals("process.runtime.jvm.memory.usage")
-                                || metricName.equals("process.runtime.jvm.memory.limit")
-                            ) {
-                                MetricDTO metricDTO = new MetricDTO();
-
-                                if (metric.getSum() != null) {
-                                    MetricSum metricSum = metric.getSum();
-                                    List<SumDataPoint> sumDataPoints = metricSum.getDataPoints();
-                                    Long value = 0L;
-
-                                    for (SumDataPoint sumDataPoint : sumDataPoints) {
-                                        Long memoryUsage = 0L;
-                                        String startTimeUnixNano = sumDataPoint.getStartTimeUnixNano();
-                                        LocalDateTime createdTime = convertUnixNanoToLocalDateTime(startTimeUnixNano);
-                                        System.out.println("---serviceName:-- " + serviceName);
-                                        System.out.println("---createdTime:-- " + createdTime);
-
-                                        if (
-                                            metricName.equals("process.runtime.jvm.memory.usage")
-                                            || metricName.equals("process.runtime.jvm.memory.limit")
-                                        ) 
-                                            if (sumDataPoint.getAsInt() != null && !sumDataPoint.getAsInt().isEmpty()) {
-                                                String asInt = sumDataPoint.getAsInt();
-                                                memoryUsage += Long.parseLong(asInt);
+        List<MetricDTO> metricDTOs = new ArrayList<>();
+        try {
+            for (ResourceMetric resourceMetric : metrics.getResourceMetrics()) {
+                String serviceName = getServiceName(resourceMetric);
+                for (ScopeMetric scopeMetric : resourceMetric.getScopeMetrics()) {
+                    try {
+                        String name = scopeMetric.getScope().getName();
+                        if (name != null && name.contains("io.opentelemetry.runtime")) {
+                            List<Metric> metricsList = scopeMetric.getMetrics();
+                            for (Metric metric : metricsList) {
+                                String metricName = metric.getName();
+                                if (
+                                    metricName.equals("process.runtime.jvm.threads.count")
+                                    || metricName.equals("process.runtime.jvm.system.cpu.utilization")
+                                    || metricName.equals("process.runtime.jvm.system.cpu.load_1m")
+                                    || metricName.equals("process.runtime.jvm.memory.usage")
+                                    || metricName.equals("process.runtime.jvm.memory.limit")
+                                ) {
+                                    LocalDateTime createdTime = null; 
+                                    Long memoryUsage = 0L;
+                                    Long cpuUsage = 0L; 
+        
+                                    if (metric.getSum() != null) {
+                                        MetricSum metricSum = metric.getSum();
+                                        List<SumDataPoint> sumDataPoints = metricSum.getDataPoints();
+        
+                                        for (SumDataPoint sumDataPoint : sumDataPoints) {
+                                            String startTimeUnixNano = sumDataPoint.getTimeUnixNano();
+                                            createdTime = convertUnixNanoToLocalDateTime(startTimeUnixNano);
+        
+                                            if (
+                                                metricName.equals("process.runtime.jvm.memory.usage")
+                                                || metricName.equals("process.runtime.jvm.memory.limit")
+                                            ) {
+                                                if (sumDataPoint.getAsInt() != null && !sumDataPoint.getAsInt().isEmpty()) {
+                                                    String asInt = sumDataPoint.getAsInt();
+                                                    memoryUsage += Long.parseLong(asInt);
+                                                }
                                             }
-                                            metricDTO.setMemoryUsage(memoryUsage);
                                         }
+                                        if (metric.getGauge() != null) {
+                                            MetricGauge metricGauge = metric.getGauge();
+                                            List<GaugeDataPoint> gaugeDataPoints = metricGauge.getDataPoints();
+                                        
+                                            for (GaugeDataPoint gaugeDataPoint : gaugeDataPoints) {
+                                                if (metricName.equals("process.runtime.jvm.cpu.utilization")) {
+                                                    if (gaugeDataPoint.getAsDouble() != null) {
+                                                        String asDouble = gaugeDataPoint.getAsDouble();
+                                                        System.out.println("---cpu------------" + asDouble);
+                                                        double cpuUsagePercentage = Double.parseDouble(asDouble) * 100;
+                                                        long roundedCpuUsage = Math.round(cpuUsagePercentage);
+                                                        cpuUsage = Long.valueOf(roundedCpuUsage);
 
-                                    } else if (metric.getHistogram() != null) {
-                                    MetricHistogram metricHistogram = metric.getHistogram();
-                                    List<HistogramDataPoint> histogramDataPoints = metricHistogram.getDataPoints();
-                                    System.out.println("HistogramDataPoints: " + histogramDataPoints);
-                                } else if (metric.getGauge() != null) {
-                                   MetricGauge metricGauge = metric.getGauge();
-                                    List<GaugeDataPoint> gaugeDataPoints = metricGauge.getDataPoints();
-                                    System.out.println("GaugeDataPoint: " + gaugeDataPoints);
-                                } else {
-                                    System.out.println("No specific metric type found for: " + metricName);
+                                                        System.out.println("------Calculated CPU Usage:------- " + cpuUsage); // Add this line for debugging
+                                                    }
+                                                }                                            
+                                            }
+                                        }
+                                        
+                                        
+                                        
+                                        MetricDTO metricDTO = new MetricDTO();
+                                        metricDTO.setMemoryUsage(memoryUsage);
+                                        metricDTO.setDate(createdTime); 
+                                        metricDTO.setServiceName(serviceName);
+                                        metricDTO.setCpuUsage(cpuUsage); 
+                                        metricDTOs.add(metricDTO);
+                                    }
                                 }
-                                
-                                metricDTOs.add(metricDTO);
                             }
                         }
+                    } catch (NullPointerException e) {
                     }
-                } catch (NullPointerException e) {
                 }
             }
+            
+        if (!metricDTOs.isEmpty()) {
+            // Persist the data
+            metricDtoRepo.persist(metricDTOs);
+            System.out.println("MetricDTOs:----------------- " + metricDTOs);
         }
         } catch (Exception e) {
-    }
-    if (!metricDTOs.isEmpty()) {
-            metricDtoRepo.persist(metricDTOs);
-      
         }
-    return metricDTOs;
-}
-
+    
+        return metricDTOs;
+    }
+    
+    
 private String getServiceName(ResourceMetric resourceMetric){
     return resourceMetric
     .getResource()
