7ca2873f1e4095c4b1455717962943be2a484621
umamuthu94 <umamuthu1994@gmail.com>
2024-02-05 14:31:07 +0530

    created a entity for pod level info

--- src/main/java/com/zaga/kafka/consumer/KeplerConsumerService.java
+++ src/main/java/com/zaga/kafka/consumer/KeplerConsumerService.java
@@ -14,7 +14,7 @@ import com.zaga.entity.kepler.KeplerMetric;
 import com.zaga.entity.otelmetric.OtelMetric;
 import com.zaga.entity.queryentity.kepler.KeplerMetricDTO;
 import com.zaga.handler.KeplerMetricCommandHandler;
-import com.zaga.handler.PodCommandHandler;
+// import com.zaga.handler.PodCommandHandler;
 
 import jakarta.inject.Inject;
 
@@ -36,37 +36,37 @@ public class KeplerConsumerService {
             System.out.println("Received null message. Check serialization/deserialization.");
         }
     }
-
-    public static void main(String[] ar) throws URISyntaxException {
-
-        Gson gson = new Gson();
-
-        File file = new File("pod.json");
-
-        try (Reader reader1 = new FileReader(file)) {
-
-            OtelMetric keplerMetric = gson.fromJson(reader1, OtelMetric.class);
-
-            // KeplerMetricCommandHandler keplerMetricCommandHandler = new KeplerMetricCommandHandler();
-            PodCommandHandler podCommandHandler = new PodCommandHandler();
-
-            List<PodMetricDTO> podMetricDTOlst = podCommandHandler.extractAndMapData(keplerMetric);
-
-            System.out.println(podMetricDTOlst.size());
-
-            for (PodMetricDTO podMetricDTO : podMetricDTOlst) {
-
-                System.out.println(podMetricDTO.toString());
-
-            }
-            System.out.println(podMetricDTOlst.size());
-
-            // keplerMetricCommandHandler.createKeplerMetric(keplerMetric);
-
-        } catch (IOException e) {
-            System.out.println("ERROR " + e.getLocalizedMessage());
-        }
-
-    }
-
 }
+    // public static void main(String[] ar) throws URISyntaxException {
+
+//         Gson gson = new Gson();
+
+//         File file = new File("pod.json");
+
+//         try (Reader reader1 = new FileReader(file)) {
+
+//             OtelMetric keplerMetric = gson.fromJson(reader1, OtelMetric.class);
+
+//             // KeplerMetricCommandHandler keplerMetricCommandHandler = new KeplerMetricCommandHandler();
+//             PodCommandHandler podCommandHandler = new PodCommandHandler();
+
+//             List<PodMetricDTO> podMetricDTOlst = podCommandHandler.extractAndMapData(keplerMetric);
+
+//             System.out.println(podMetricDTOlst.size());
+
+//             for (PodMetricDTO podMetricDTO : podMetricDTOlst) {
+
+//                 System.out.println(podMetricDTO.toString());
+
+//             }
+//             System.out.println(podMetricDTOlst.size());
+
+//             // keplerMetricCommandHandler.createKeplerMetric(keplerMetric);
+
+//         } catch (IOException e) {
+//             System.out.println("ERROR " + e.getLocalizedMessage());
+//         }
+
+//     }
+
+// }
