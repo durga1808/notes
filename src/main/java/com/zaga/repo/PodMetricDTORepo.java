70597592afa600cb74a5f2dd2910468f3fc68f4c
Anushiya <anushiyadhamu@gmail.com>
2024-02-05 14:20:59 +0530

    update the pod level cluster info

---
+++ src/main/java/com/zaga/repo/PodMetricDTORepo.java
@@ -0,0 +1,9 @@
+package com.zaga.repo;
+
+import com.zaga.entity.PodMetricDTO;
+
+import io.quarkus.mongodb.panache.PanacheMongoRepository;
+
+public class PodMetricDTORepo implements PanacheMongoRepository<PodMetricDTO>{
+    
+}
