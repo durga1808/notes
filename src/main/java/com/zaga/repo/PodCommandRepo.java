49e12dc6bc72c524ea9672ec385eed1f04791819
umamuthu94 <umamuthu1994@gmail.com>
2024-02-05 14:24:05 +0530

    created a entity for pod level info

--- src/main/java/com/zaga/repo/PodCommandRepo.java
+++ src/main/java/com/zaga/repo/PodCommandRepo.java
@@ -1,11 +1,12 @@
 package com.zaga.repo;
 
 import com.zaga.entity.otelmetric.OtelMetric;
+import com.zaga.entity.pod.OtelPodMetric;
 
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class PodCommandRepo implements PanacheMongoRepository<OtelMetric> {
+public class PodCommandRepo implements PanacheMongoRepository<OtelPodMetric> {
     
 }
